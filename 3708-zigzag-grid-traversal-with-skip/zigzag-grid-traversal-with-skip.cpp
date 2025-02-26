aec817659b4720cd8d52650ae5f66a0f60de146e
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2025-02-26 11:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 32.8 MB (7.04%) - LeetSync

---
+++ 3708-zigzag-grid-traversal-with-skip/zigzag-grid-traversal-with-skip.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> zigzagTraversal(vector<vector<int>>& grid) 
+    {
+        vector<int> ans;
+        bool goingright = true;
+        int r =0,c=0;
+        while(r<grid.size())
+            {
+                if(goingright)
+                {
+                    for(int i = 0 ; i < grid[0].size();i++)
+                        {
+                            ans.push_back(grid[r][i]);
+                        }
+                    c = grid[0].size()-1;
+                    r++;
+                    goingright=false;
+                }
+                else
+                {
+                    for(int i = c ; i >=0 ; i--)
+                        {
+                            ans.push_back(grid[r][i]);
+                        }
+                    r++;
+                    goingright=true;
+                }
+            }
+        vector<int> finalans;
+        for(int i =0 ; i< ans.size();i+=2)
+                finalans.push_back(ans[i]);
+        return finalans;
+    }
+};
