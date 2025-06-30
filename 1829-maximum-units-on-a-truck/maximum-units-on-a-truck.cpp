807720f43e54a60d6b6781a51a552a0f84494c45
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2025-06-30 06:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (90.19%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    // Comparator to sort by units per box in descending order
     static bool comp(vector<int>& box1, vector<int>& box2){
         return box1[1] > box2[1];  
     }
@@ -8,18 +7,17 @@ public:
         int n=boxTypes.size();
         int remSize=truckSize;
         int totUnits = 0;
-        // Sort by units per box in descending order
-        sort(boxTypes.begin(), boxTypes.end(), comp);
-        for(int i = 0; i < n; i++){
-            if(remSize == 0) break;
-            if(boxTypes[i][0] <= remSize){
+        sort(boxTypes.begin(), boxTypes.end(), comp);// Sort by units per box in descending order
+        for(int i=0;i<n;i++){
+            if(remSize==0) break;
+            if(boxTypes[i][0]<=remSize){
                 // Take all boxes of this type
-                remSize -= boxTypes[i][0];
-                totUnits += boxTypes[i][0] * boxTypes[i][1];
+                remSize-=boxTypes[i][0];
+                totUnits+=boxTypes[i][0]*boxTypes[i][1];
             }else {
                 // Take only the remaining boxes we can fit
-                totUnits += remSize * boxTypes[i][1];
-                remSize = 0;  // Truck is full
+                totUnits+=remSize*boxTypes[i][1];
+                remSize=0;  // Truck is full
             }
         }
         return totUnits;
