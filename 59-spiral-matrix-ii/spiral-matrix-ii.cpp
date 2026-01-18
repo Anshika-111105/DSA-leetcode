831d680bf4f7c3d411a13567d9476ca829a0b9a4
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-01-18 22:35:40 +0530

    Time: 1 ms (9.60%) | Memory: 9.6 MB (38.36%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,53 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> matrix;
-        int temp = 1;
-        for(int i = 0; i < n; i++){
-            matrix.push_back({});
-            for(int j = 0; j < n; j++){
-                matrix[i].push_back(temp);
-                temp++;
+        vector<vector<int>> vec(n,vector<int>(n));
+        vector<vector<bool>> con(n,vector<bool>(n, false));
+        int no = 1;
+        int i = 0;
+        int j = 0;
+        while(no <= n*n)
+        {
+            while((i < n && j < n) && con[i][j] == false)
+            {
+                vec[i][j] = no;
+                no++;
+                con[i][j] = true;
+                j++;
             }
+            i++;
+            j--;
+            while((i < n && j < n) && con[i][j] == false)
+            {
+                vec[i][j] = no;
+                no++;
+                con[i][j] = true;
+                i++;
+            }
+            j--;
+            i--;
+            while((i < n && j >= 0) && con[i][j] == false)
+            {
+                vec[i][j] = no;
+                no++;
+                con[i][j] = true;
+                j--;
+            }
+            i--;
+            j++;
+            while((i >= 0 && j < n) && con[i][j] == false)
+            {
+                vec[i][j] = no;
+                no++;
+                con[i][j] = true;
+                i--;
+            }
+            i++;
+            j++;
         }
-
-        int top = 0, left = 0;
-        int bottom = n - 1, right = n - 1;
-        int idx = 1;
-        while (top <= bottom && left <= right) {
-
-            // right
-            for (int i = left; i <= right; ++i) {
-                matrix[top][i] = idx;
-                idx++;
-            }
-            top++;
-            
-            // down
-            for (int i = top; i <= bottom; ++i) {
-                matrix[i][right] = idx;
-                idx++;
-            }
-            right--;
-            
-            // left
-            if (top <= bottom) {
-                for (int i = right; i >= left; --i) {
-                    matrix[bottom][i] = idx;
-                    idx++;
-                }
-                bottom--;
-            }
-            
-            // up
-            if (left <= right) {
-                for (int i = bottom; i >= top; --i) {
-                    matrix[i][left] = idx;
-                    idx++;
-                }
-                left++;
-            }
-        }
-        return matrix;
+        return vec;
     }
 };
