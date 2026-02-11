e6c95f94e40c76ff11a0ebe181a6abb452886e59
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-02-11 23:21:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.82%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows=matrix.size(),cols=matrix[0].size();
-        int row=0,col=cols-1;
-        while(row<rows && col>-1){
-            int curr = matrix[row][col];
-            if(curr == target) return true;
-            if(target>curr){
-                row++;
+    bool searchRow(vector<vector<int>>& matrix, int target,int row){
+        int n=matrix[0].size();
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(target==matrix[row][mid])return true;
+            else if(target>matrix[row][mid]){
+                low=mid+1;
             }else{
-                col--;
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size(); //no. of rows
+        int n=matrix[0].size();//no of columns
+        int startRow=0,endRow=m-1;
+        while(startRow<=endRow){
+            int mid=startRow+(endRow-startRow)/2; //to avoid Integer Overflow
+            if(target>=matrix[mid][0] && target<=matrix[mid][n-1]){
+                //found the row => BS on that row
+                return searchRow(matrix,target,mid);
+            }else if(target>=matrix[mid][n-1]){
+                startRow=mid+1;
+            }else{
+                endRow=mid-1;
             }
         }
         return false;
