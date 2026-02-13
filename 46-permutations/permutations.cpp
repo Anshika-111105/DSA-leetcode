23ab10422fad2aff2c2177fe83911fc614439955
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-02-13 08:57:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.86%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     void perms(int i,vector<vector<int>>& res,vector<int>& nums){ //res=>by reference 
         if(i==nums.size()){
-            res.push_back(nums);
+            res.push_back({nums});
             return;
         }
 
         for(int j=i;j<nums.size();j++){
-            swap(nums[i],nums[j]); //alteration is in the original array 
+            swap(nums[i],nums[j]); //alteration is in the original array =>index element-ith choice
             perms(i+1,res,nums);
             swap(nums[i],nums[j]);//backtracking
         }
