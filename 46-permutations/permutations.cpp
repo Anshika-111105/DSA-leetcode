0ece779234c0223b14a86283b4c3b43e2dfe94d9
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-02-13 08:54:55 +0530

    Time: 2 ms (37.65%) | Memory: 10.5 MB (87.86%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void perms(int i,vector<vector<int>>& res,vector<int>& nums){ //res=>by reference 
+        if(i==nums.size()){
+            res.push_back(nums);
+            return;
+        }
+
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]); //alteration is in the original array 
+            perms(i+1,res,nums);
+            swap(nums[i],nums[j]);//backtracking
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        perms(0,result,nums);
+        return result;
+
+
+        
+
+    }
+};
