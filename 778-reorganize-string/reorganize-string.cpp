2f47e6accaea7441df2d2bafc45af5b653257203
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-03-06 10:32:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (53.38%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        unordered_map<char,int> freq;
-        for (char c : s) freq[c]++;
-
-        priority_queue<pair<int,char>> pq;
-        for (auto &e : freq) {
-            if (e.second > (s.size() + 1) / 2)
+        vector<int> count(26, 0);
+        for(char c : s){
+            count[c - 'a']++;
+        }
+        priority_queue<pair<int,int>> pq;
+        for(int i = 0; i < 26; i++){
+            if(count[i] > 0){
+                pq.push({count[i], i});
+            }
+        }
+        string ans = "";
+        while(pq.size() > 1){
+            auto first = pq.top(); pq.pop();
+            auto second = pq.top(); pq.pop();
+            int f1 = first.first;
+            int c1 = first.second;
+            int f2 = second.first;
+            int c2 = second.second;
+            ans += char(c1 + 'a');
+            ans += char(c2 + 'a');
+            if(--f1 > 0){
+                pq.push({f1, c1});
+            }
+            if(--f2 > 0){
+                pq.push({f2, c2});
+            }
+        }
+        if(!pq.empty()){
+            auto last = pq.top();pq.pop();
+            int freq = last.first;
+            int ch = last.second;
+            if(freq > 1){
                 return "";
-            pq.push({e.second, e.first});
+            }
+            ans += char(ch + 'a');
         }
 
-        string result = "";
-        pair<int,char> prev = {0, '#'}; // previous used character
-
-        while (!pq.empty()) {
-            auto curr = pq.top();
-            pq.pop();
-
-            result += curr.second;
-            curr.first--;
-
-            // push previous back AFTER using current
-            if (prev.first > 0)
-                pq.push(prev);
-
-            prev = curr;
-        }
-
-        return result;
+        return ans;
     }
 };
