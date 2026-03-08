d2a12c5ac36571306e423d466a2b1206bd1fb37f
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-03-08 09:29:02 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (35.45%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -3,7 +3,6 @@ public:
     string ans = "";
     string curr = "";
     unordered_set<string> st;
-
     void helper(int n) {
         if(curr.size() == n) {
             if(!st.count(curr)) {
@@ -11,25 +10,19 @@ public:
             }
             return;
         }
-
         if(ans != "") return;
-
         curr.push_back('0');
         helper(n);
         curr.pop_back();
-
         curr.push_back('1');
         helper(n);
         curr.pop_back();
     }
-
     string findDifferentBinaryString(vector<string>& nums) {
         int n = nums.size();
-
         for(auto &s : nums) {
             st.insert(s);
         }
-
         helper(n);
         return ans;
     }
