d246210cbf11ee4278bf5f1d7718d2d000a23b66
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-03-20 10:03:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (90.34%) - LeetSync

---
+++ 1279-prime-arrangements/prime-arrangements.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    long long permutation(int count,int MOD){
+        long long ans=1;
+        for(int i=2;i<=count;i++){
+            ans=(ans*i)%MOD;
+        }
+        return ans;
+    }
+public:
+    int numPrimeArrangements(int n) {
+        int MOD= 1e9 + 7;
+        int count=0;
+        for(int i=2;i<=n;i++){
+            bool isPrime=true;
+            for(int j=2;j<=sqrt(i);j++){
+                if(i%j==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                count++;
+            }
+        }
+        return (permutation(count,MOD)*permutation(n-count,MOD))%MOD;
+
+
+    }
+};
