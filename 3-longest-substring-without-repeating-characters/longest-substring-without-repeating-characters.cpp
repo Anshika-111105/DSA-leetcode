82d8f58bbd9beb9e2c81f6d930d5de07df864696
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-03-28 07:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (75.42%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,8 +5,6 @@ public:
         if(n==0)return 0;
         int l=0,r=0,len=r-l+1;
         int maxlen=0;
-        //int hash[256];
-        //fill(hash, hash + 256, -1);
         vector<int> hash(256,-1);
         while(r<n){
             if(hash[s[r]]!=-1){ //in the map
@@ -22,3 +20,24 @@ public:
         return maxlen;
     }
 };
+/*class solution{
+public:
+    int lengthOfLongestSubstring(string s){
+        int n=s.length();
+        int l=0,r=0,l=0, maxlen=0;
+        if(n==0)return 0; //edge case
+        vector<int> freq(256,-1);
+        while(r<n){
+            if(freq[s[r]]!=-1){
+                if(freq[s[r]]>=l){
+                    l=freq[s[r]]+1;
+                }
+            }
+            len=r-l+1;
+            maxlen=max(len,maxlen);
+            freq[s[r]]=r;
+            r++;
+        }
+        return
+    }
+}*/
