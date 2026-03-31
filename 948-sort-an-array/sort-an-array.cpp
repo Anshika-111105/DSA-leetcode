adc13b28aa28e440047a4d2bbba8e9cf6fcee351
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-04-01 00:12:05 +0530

    Time: 242 ms (52.94%) | Memory: 146.6 MB (44.99%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
-    void merge(vector<int>& nums,int low,int mid,int high){
-        int left=low;
-        int right=mid+1;
-        vector<int>temp;
-        while(left<=mid && right<=high){
-            if(nums[left]<=nums[right]){
-                temp.push_back(nums[left]);
-                left++;
-            }else{
-                temp.push_back(nums[right]);
-                right++;
+    void conquer(vector<int>& arr, int start, int mid, int end) {
+        vector<int> mergeArray(end - start + 1);
+
+        int idx1 = start;
+        int idx2 = mid + 1;
+        int x = 0;
+
+        while (idx1 <= mid && idx2 <= end) {
+            if (arr[idx1] <= arr[idx2]) {
+                mergeArray[x++] = arr[idx1++];
+            } else {
+                mergeArray[x++] = arr[idx2++];
             }
         }
-            while(left<=mid){
-                temp.push_back(nums[left]);
-                left++;
-            }
-            while(right<=high){
-                temp.push_back(nums[right]);
-                right++;
-            }
-            for(int i=0;i<temp.size();i++){
-                nums[low+i]=temp[i];
+
+        while (idx1 <= mid) {
+            mergeArray[x++] = arr[idx1++];
+        }
+
+        while (idx2 <= end) {
+            mergeArray[x++] = arr[idx2++];
+        }
+
+        for (int i = 0; i < mergeArray.size(); i++) {
+            arr[start + i] = mergeArray[i];
         }
     }
-    void mergesort(vector<int>& nums,int low,int high){
-        if(low==high){
-            return;
-        }
-        int mid=low+(high-low)/2;
-        mergesort(nums,low,mid);
-        mergesort(nums,mid+1,high);
-        merge(nums,low,mid,high);
+
+    void divide(vector<int>& arr, int start, int end) {
+        if (start >= end) return;
+
+        int mid = start + (end - start) / 2;
+
+        divide(arr, start, mid);
+        divide(arr, mid + 1, end);
+
+        conquer(arr, start, mid, end);
     }
+
     vector<int> sortArray(vector<int>& nums) {
-        mergesort(nums,0,nums.size()-1);
+        divide(nums, 0, nums.size() - 1);
         return nums;
     }
 };
