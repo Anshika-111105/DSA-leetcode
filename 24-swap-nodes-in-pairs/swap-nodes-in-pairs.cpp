024e5200be7bb7a74c1b2d2f0636c3968cc12bbe
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-04-02 08:37:05 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (99.06%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -1,18 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        // if head is NULL OR just having a single node, then no need to change anything 
-        if(head == NULL || head -> next == NULL) 
-        {
-            return head;
+    if (!head || !head->next) return head;
+    ListNode* newHead = head->next;  // new head will be 2nd node
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+    while (curr && curr->next) {
+        ListNode* nextPair = curr->next->next;
+        ListNode* second = curr->next;
+        // swap
+        second->next = curr;
+        curr->next = nextPair;
+        // connect previous pair
+        if (prev != NULL) {
+            prev->next = second;
         }
-            
-        ListNode* temp; // temporary pointer to store head -> next
-        temp = head->next; // give temp what he want
-        
-        head->next = swapPairs(head->next->next); // changing links
-        temp->next = head; // put temp -> next to head
-        
-        return temp; // now after changing links, temp act as our head
+        // move pointers forward
+        prev = curr;
+        curr = nextPair;
     }
+
+    return newHead;
+}
+    
 };
