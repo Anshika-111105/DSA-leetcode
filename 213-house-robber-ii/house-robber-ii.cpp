de9dd62e729e8e822396a63d1b02f658e4f296c1
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-04-18 10:56:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (86.89%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        
+        // Rob houses 0 to n-2 OR houses 1 to n-1
+        auto robRange = [](vector<int>& nums, int start, int end) {
+            int prev2 = 0, prev1 = 0;
+            for (int i = start; i <= end; i++) {
+                int curr = max(prev1, prev2 + nums[i]);
+                prev2 = prev1;
+                prev1 = curr;
+            }
+            return prev1;
+        };
+        
+        return max(robRange(nums, 0, n - 2), robRange(nums, 1, n - 1));
+    }
+};
