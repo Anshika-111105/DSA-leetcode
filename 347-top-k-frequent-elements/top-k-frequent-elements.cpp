5bc7cc85144cefd0f3ba4eef6ea2597199eb43ab
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-04-27 23:02:13 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (38.63%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        for (int x : nums) {
-            mp[x]++;
+        unordered_map<int, int> freq;
+
+        // Step 1: Count frequency
+        for(int x : nums){
+            freq[x]++;
         }
-        vector<pair<int, int>> v(mp.begin(), mp.end());
-        sort(v.begin(), v.end(), [](pair<int, int>& a, pair<int, int>& b) {
-            return a.second > b.second;
-        });
-        vector<int>ans(k);
-        for(int i = 0; i<k;i++){
-            ans[i] = v[i].first;
+
+        // Step 2: Min heap (freq, element)
+        priority_queue<
+            pair<int,int>,
+            vector<pair<int,int>>,
+            greater<pair<int,int>>
+        > min_heap;
+
+        // Step 3: Keep only top k elements
+        for(auto &it : freq){
+            min_heap.push({it.second, it.first});
+            if(min_heap.size() > k){
+                min_heap.pop();
+            }
         }
+
+        // Step 4: Extract result
+        vector<int> ans;
+        while(!min_heap.empty()){
+            ans.push_back(min_heap.top().second);
+            min_heap.pop();
+        }
+
         return ans;
     }
 };
