7c7327f0aca424ec630003a41bfc747b0c4b2cd0
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-04-28 09:13:46 +0530

    Time: 12 ms (90.12%) | Memory: 78.8 MB (91.36%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int minOperations(vector<vector<int>>& grid, int x) {
-        const int m=grid.size(), n=grid[0].size(), N=m*n;
-        vector<int> val(N,0);
-        int r=grid[0][0]%x,i=0;
+    static int minOperations(vector<vector<int>>& grid, int x) {
+        const int m = grid.size(), n = grid[0].size(), N = m * n;
+        int freq[10001] = {0}, xMin = INT_MAX, xMax = 0;
+
+        int r = grid[0][0] % x;
         for (const auto& row : grid) {
             for (int num : row) {
                 auto [q, rr] = div(num, x);
                 if (rr != r)
-                    return -1;
-                val[i++]=q;
+                    return -1; // If not consistent, return -1
+                freq[q]++;  
+                xMax=max(xMax, q);
+                xMin=min(xMin, q);
             }
         }
-        sort(val.begin(), val.end());
-        int med=(N&1)?val[N/2] : (val[N/2-1] + val[N/2])/2;
-        int op = 0;
-        for (int num : val)
-            op+=abs(num-med);
-
+        int op=0;
+        for (int l=xMin, r=xMax; l<r; ) {
+            while(l<r && freq[l]==0) l++;
+            while(l<r && freq[r]==0) r--;
+            op+=r-l;
+            if (--freq[l]==0) l++;
+            if (--freq[r]==0) r--;
+        }
         return op;
     }
 };
