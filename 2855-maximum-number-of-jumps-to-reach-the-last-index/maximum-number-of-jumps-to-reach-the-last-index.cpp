f1337d5c6f8fe946817215a3ed34174fd45a2af2
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-05-10 19:57:48 +0530

    Time: 25 ms (30.75%) | Memory: 68.4 MB (69.25%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,48 +1,22 @@
-// Top-Down DP + Memoization Approach
-
 class Solution {
 public:
-    int n;
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+        // dp array to store max jumps till index i
+        vector<int> dp(n, -1);
 
-    // Recursive function to find maximum jumps from index i
-    int solve(int i, vector<int>& nums, int target, vector<int>& dp) {
+        // First index is reachable with 0 jumps
+        dp[0]=0;
 
-        // If we reach last index, no more jumps needed
-        if(i == n - 1)
-            return 0;
-
-        // Return already computed result
-        if(dp[i] != -2)
-            return dp[i];
-
-        int ans = -1;
-
-        // Try all possible next jumps
-        for(int j = i + 1; j < n; j++) {
-
-            // Check jump condition
-            if(abs(nums[j] - nums[i]) <= target) {
-
-                int temp = solve(j, nums, target, dp);
-
-                // Update maximum jumps
-                if(temp != -1) {
-                    ans = max(ans, 1 + temp);
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                // Check if the condition holds true and prev index was visited
+                if(abs(nums[i]-nums[j]) <= target && dp[j]>-1) {
+                    dp[i] = max(dp[i], 1+dp[j]);
                 }
             }
         }
 
-        // Store and return answer
-        return dp[i] = ans;
-    }
-
-    int maximumJumps(vector<int>& nums, int target) {
-
-        n = nums.size();
-
-        // -2 means not calculated yet
-        vector<int> dp(n, -2);
-
-        return solve(0, nums, target, dp);
+        return dp[n-1];
     }
 };
