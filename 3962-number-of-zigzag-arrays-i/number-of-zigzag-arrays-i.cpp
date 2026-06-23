9d0f06bbc8a407369afca83c8d34658d86c51e70
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-06-23 22:43:43 +0530

    Time: 260 ms (75.00%) | Memory: 11.8 MB (74.07%) - LeetSync

--- 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.cpp
+++ 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.cpp
@@ -4,7 +4,6 @@ public:
     int zigZagArrays(int n, int l, int r) {
         int m = r - l + 1;
         vector<int> dp(m, 1);
-
         for (int i = 2; i <= n; i++) {
             reverse(dp.begin(), dp.end());
             int sum = 0;
