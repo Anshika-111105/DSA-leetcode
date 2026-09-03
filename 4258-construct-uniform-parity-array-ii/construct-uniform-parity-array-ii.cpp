be887a73e389598b0584c74e3a698417353653aa
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2026-09-03 22:40:51 +0530

    Time: 0 ms (100.00%) | Memory: 165.8 MB (62.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-    bool uniformArray(auto& A) {
-        uint32_t x[2] = {-1u, -1u};
-
-        for (uint32_t a : A)
-            x[a & 1] = min(x[a & 1], a);
-
-        //TRUE IF: The min is odd || ∄ Odd elements  
-        return x[1] < x[0] | x[1] == -1u;
+    bool uniformArray(vector<int>& nums1) {
+        // Find the minimum element in the array
+        int mn = *min_element(nums1.begin(), nums1.end());
+        
+        // If the minimum element is odd, we can make all elements odd
+        if (mn % 2 == 1) {
+            return true;
+        } else {
+            // If the minimum is even, all elements MUST already be even
+            for (int i = 0; i < nums1.size(); i++) {
+                if (nums1[i] % 2 == 1) {
+                    return false; // Found an odd number, impossible to convert
+                }
+            }
+        }
+        return true;
     }
 };
